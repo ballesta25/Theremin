25761f25b8cf282cd930060a103c5baa76c29d97
ballesta25 <ballesta25@gmail.com>
2023-11-03 16:01:13 -0500

    begin adding object language grammar rules as `egg` rewrites

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
 pub mod sygus;
 
-use egg::*;
+use egg::{rewrite as rw, *};
+use once_cell::sync::Lazy;
+
+pub type SLIALang = SymbolLang;
+pub type Spec = ();
+
+static grammar_rules: Lazy<[Rewrite<SLIALang, Spec>; 5]> = Lazy::new(|| {
+    [
+        rw!("eq"; "(Bool n)" => "(= (S(n)) int int)"),
+        rw!("gt"; "(Bool n)" => "(> (S(n)) int int)"),
+        rw!("ge"; "(Bool n)" => "(>= (S(n)) int int)"),
+        rw!("lt"; "(Bool n)" => "(< (S(n)) int int)"),
+        rw!("le"; "(Bool n)" => "(<= (S(n)) int int)"),
+    ]
+});
+
+fn build_egraph(examples: Spec) -> EGraph<SLIALang, Spec> {
+    let mut graph: EGraph<SLIALang, Spec> = Default::default();
+
+    graph
+}
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
