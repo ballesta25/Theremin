dee6a293fee3bb8e2317d6232c942d27c8041900
Dominic Tassio <dominic.tassio@gmail.com>
2023-11-04 18:48:37 -0500

    Add main to demonstrate bottom up enumeration

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
-use pest::Parser;
-use std::{env, fs};
-use theremin::sygus::{Rule, SygusParser};
+use std::{env, fs, process::exit};
+use theremin::{enumerate::bottom_up, sygus::parse_sygus_file};
 
 fn main() {
-    let args: Vec<String> = env::args().take(2).collect();
-    if args.len() != 2 {
-        println!("Usage: theremin <path>");
+    let args: Vec<String> = env::args().take(3).collect();
+    if args.len() != 3 {
+        println!("Usage: theremin <path> <depth>");
         return;
     }
 
     let file = fs::read_to_string(&args[1]).expect("cannot read file");
-    let result = SygusParser::parse(Rule::sygus, &file);
+    let conjecture = parse_sygus_file(&file);
 
-    match result {
-        Err(e) => println!("ERROR: {}", e),
-        Ok(spec) => println!("{:#?}", spec),
+    match conjecture {
+        Err(err) => println!("{:#?}", err),
+        Ok(conjecture) => {
+            for function in conjecture.functions_to_synthesize {
+                let bank = bottom_up(
+                    function.grammar,
+                    args[2].parse().expect("depth should be a number"),
+                );
+                println!("; {}", bank.len());
+                for term in bank {
+                    println!("{}", term);
+                }
+                exit(0);
+            }
+        }
     }
 }
