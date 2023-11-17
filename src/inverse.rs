5375b87b204ced62b1e3b3dfaf482aac3af8ce7e
BRiches423 <bdrxbc@umsystem.edu>
2023-11-17 13:50:14 -0600

    inverse semantics now compiling

--- src/inverse.rs
+++ src/inverse.rs
@@ -4,7 +4,35 @@ use crate::language::Expr;
 
 
 
-pub fn invert(spec : Spec, symbol : &str) -> Spec {
+fn test_valid (example : &Option<(Expr, Expr)>) -> bool {
+
+    match example {
+
+        None => true,
+        _ => false,
+    }
+
+}
+
+
+fn unwrap_vec (vecs : Vec<Option<(Expr, Expr)>>) -> Spec {
+
+    if vecs.iter().any(test_valid) {
+
+        Spec::Impossible
+
+    }else{
+
+        Spec::Examples( vecs.into_iter().map( |a: Option<(Expr,Expr)>| a.unwrap()).collect())
+
+    } 
+
+}
+
+
+
+
+pub fn invert(spec : &Spec, symbol : &str) -> Spec {
 
    match spec {
 
@@ -13,65 +41,111 @@ pub fn invert(spec : Spec, symbol : &str) -> Spec {
         match symbol {
 
 
-            substring0 => {
+            "substring0" => {
 
-                a.iter().map( |Expr::ConstStr(i) , Expr::ConstStr(o)| (Expr::ConstStr(i), Expr::ConstStr(i))).collect()
+                let temp : Vec<Option<(Expr, Expr)>>  = a.iter().map(|(i , o)| {
+                    
+                    match (i,o) {
+                        
+                        (Expr::ConstStr(inn), Expr::ConstStr(out)) => {
+                            
+                            if let Some(_) = inn.find(out) {
+                                Some((Expr::ConstStr(inn.clone()), Expr::ConstStr(inn.clone())))
+                            } else {
+                                None
+                            }
+                            
+                        }, 
+                        _ => None
+
+                    }
+                
+                
+                } ).collect();
+                
+                unwrap_vec(temp)
 
             },
 
-            substring1 => {
+            "substring1" => {
 
                 
-                
-               let mut temp = a.iter().map( |(Expr::ConstStr(i), Expr::ConstStr(o))| (Expr::ConstStr(i), Expr::ConstInt(
-                    if let Some(k) = i.find(o) {
-                        k as i32
-                    } else {
-                        -1 as i32
+                let temp : Vec<Option<(Expr, Expr)>>  = a.iter().map(|(i , o)| {
+                    
+                    match (i,o) {
+                        
+                        (Expr::ConstStr(inn), Expr::ConstStr(out)) => {
+                            
+                            if let Some(k) = inn.find(out) {
+                                Some((i.clone(), Expr::ConstInt(k as i32)))
+                            } else {
+                                None
+                            }
+                            
+                        }, 
+                        _ => None
+
                     }
-                    ))).collect(); //we are relaxing inverse requirement by assuming first occurence is only occurence
+                
+                
+                } ).collect();
+                
+                unwrap_vec(temp)
 
-               let c = temp.iter().any(|(Expr::ConstStr(i), Expr::ConstInt(o))| o < 0); // check that all specs are even possible 
-
-               if c {
-
-                Spec::Impossible
-
-               } else {
-
-                temp
-
-               }
 
             },
 
-            substring2 => {
+            "substring2" => {
 
                 
-                
-                let mut temp = a.iter().map( |(Expr::ConstStr(i), Expr::ConstStr(o))| (Expr::ConstStr(i), Expr::ConstInt(
-                    if let Some(k) = i.find(o) {
-                        k + (o.len() as i32) as i32
-                    } else {
-                        -1 as i32
+
+                let temp : Vec<Option<(Expr, Expr)>>  = a.iter().map(|(i , o)| {
+                    
+                    match (i,o) {
+                        
+                        (Expr::ConstStr(inn), Expr::ConstStr(out)) => {
+                            
+                            if let Some(k) = inn.find(out) {
+                                Some((i.clone(), Expr::ConstInt((k + out.len()) as i32)))
+                            } else {
+                                None
+                            }
+                            
+                        }, 
+                        _ => None
+
                     }
-                    ))).collect(); //we are relaxing inverse requirement by assuming first occurence is only occurence
- 
-                let c = temp.iter().any(|(Expr::ConstStr(i), Expr::ConstInt(o))| o < 0);
- 
-                if c {
- 
-                 Spec::Impossible // if the substring isn't even in there 
- 
-                } else {
- 
-                 temp
- 
-                }
+                
+                
+                } ).collect();
+                
+                unwrap_vec(temp)
+                
+               
  
              },
 
-             
+            //   append0: => {
+
+            //         this one will be interesting because it only makes sense to use append if our output is 
+            //         either consistently appended to the end or appended to the beginning but not both, 
+            //         That will be interesting. 
+            //         I will also need to be checking to see if the output is ONLY an append op. as if it isn't we will need to bla bla bla
+            //         got it. 
+            //          String.find returns an option type, so for a given entry I will assign a -1 if on left 1 if on right and 0 if not found
+            //          Or if it's more complex than a substring relation. 
+
+                 
+                
+
+                        
+
+
+
+
+            //   },
+
+
 
             _ => Spec::Indeterminate,
 
@@ -79,7 +153,7 @@ pub fn invert(spec : Spec, symbol : &str) -> Spec {
         }
 
     },
-    _ => spec,
+    _ => spec.clone(),
 
    }
 
