efa516fa05139c85a8e41ebec29c88167c873fc0
BRiches423 <bdrxbc@umsystem.edu>
2023-11-16 22:33:30 -0600

    Added some inverse functions

--- src/inverse.rs
+++ src/inverse.rs
@@ -15,9 +15,64 @@ pub fn invert(spec : Spec, symbol : &str) -> Spec {
 
             substring0 => {
 
-                a.iter().map( |(Expr::ConstStr(a), Expr::ConstStr(b))| (Expr::ConstStr(a), Expr::ConstStr(a))).collect()
+                a.iter().map( |Expr::ConstStr(i) , Expr::ConstStr(o)| (Expr::ConstStr(i), Expr::ConstStr(i))).collect()
 
             },
+
+            substring1 => {
+
+                
+                
+               let mut temp = a.iter().map( |(Expr::ConstStr(i), Expr::ConstStr(o))| (Expr::ConstStr(i), Expr::ConstInt(
+                    if let Some(k) = i.find(o) {
+                        k as i32
+                    } else {
+                        -1 as i32
+                    }
+                    ))).collect(); //we are relaxing inverse requirement by assuming first occurence is only occurence
+
+               let c = temp.iter().any(|(Expr::ConstStr(i), Expr::ConstInt(o))| o < 0); // check that all specs are even possible 
+
+               if c {
+
+                Spec::Impossible
+
+               } else {
+
+                temp
+
+               }
+
+            },
+
+            substring2 => {
+
+                
+                
+                let mut temp = a.iter().map( |(Expr::ConstStr(i), Expr::ConstStr(o))| (Expr::ConstStr(i), Expr::ConstInt(
+                    if let Some(k) = i.find(o) {
+                        k + (o.len() as i32) as i32
+                    } else {
+                        -1 as i32
+                    }
+                    ))).collect(); //we are relaxing inverse requirement by assuming first occurence is only occurence
+ 
+                let c = temp.iter().any(|(Expr::ConstStr(i), Expr::ConstInt(o))| o < 0);
+ 
+                if c {
+ 
+                 Spec::Impossible // if the substring isn't even in there 
+ 
+                } else {
+ 
+                 temp
+ 
+                }
+ 
+             },
+
+             
+
             _ => Spec::Indeterminate,
 
 
