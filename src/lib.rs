91ea9f877f2a4403998804dcf7915e1e12c920d1
ballesta25 <ballesta25@gmail.com>
2023-11-20 16:12:26 -0600

    store grammar rules as an `fn` so we don't need to keep a manual counter of number of rw rules; improve structure of `EvalCostFn::cost()`

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use egg::{rewrite as rw, *};
 use language::*;
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
+use std::iter;
 
 pub type SLIALang = SymbolLang;
 #[derive(Clone, Debug, Default)]
@@ -58,38 +59,41 @@ impl<'a> CostFunction<SLIALang> for EvalCostFn<'a> {
     where
         C: FnMut(Id) -> Self::Cost,
     {
-        let (mut holes, size) = enode.fold((0, 1), |(a, b), id| {
+        let (mut holes, mut size) = (0, 1);
+        enode.fold((0, 1), |(a, b), id| {
             let (a1, b1) = costs(id);
             (a + a1, b + b1)
         });
 
         //check if enode *is* a hole
-        match enode.op.as_str() {
-            "hole" => holes += 1,
-            _ => (),
+        let symbol = enode.op.as_str();
+        let sorts: Vec<&str> = vec!["Int", "Bool", "String"];
+        if sorts.iter().any(|&x| x == symbol) {
+            // try to fill hole - if failure increment hole counter
+            holes += 1;
+        } else {
+            // not a hole
         }
-        // eval to check for wrong examples
         (holes, size)
     }
 }
 
-static grammar_rules: Lazy<[Rewrite<SLIALang, Spec>; 6]> = Lazy::new(|| {
-    [
+pub fn grammar_rules() -> Vec<Rewrite<SLIALang, Spec>> {
+    vec![
         rw!("eq"; "(Bool ?s)" => "(= (Int (inv eq0 ?s)) (Int (inv eq1 ?s)))"),
         rw!("gt"; "(Bool ?s)" => "(> (Int (inv gt0 ?s)) (Int (inv gt1 ?s)))"),
         rw!("ge"; "(Bool ?s)" => "(>= (Int (inv ge0 ?s)) (Int (inv ge1 ?s)))"),
         rw!("lt"; "(Bool ?s)" => "(< (Int (inv lt0 ?s)) (Int (inv lt1 ?s)))"),
         rw!("le"; "(Bool ?s)" => "(<= (Int (inv le0 ?s)) (Int (inv le1 ?s)))"),
-        rw!("int_hole"; "(Int ?s)" => "(hole Int ?s)"),
     ]
-});
+}
 
 fn build_egraph(examples: Spec) -> (EGraph<SLIALang, Spec>, Runner<SLIALang, Spec>) {
     let graph: EGraph<SLIALang, Spec> = Default::default();
 
     let start: RecExpr<SLIALang> = "(Bool root_spec)".parse().unwrap();
 
-    let rules = grammar_rules.clone();
+    let rules = grammar_rules();
 
     let runner = Runner::default().with_expr(&start).run(&rules);
 
